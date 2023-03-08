ab6160d570b19f891469ba35bc52e5c0ff266787
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-08 18:48:24 -0500

    sets up basic server

---
+++ server.js
@@ -0,0 +1,22 @@
+// basic set up
+const express  = require('express');
+const routes = require('./routes');
+const app = express();
+const PORT = 3001;
+
+
+// middleware for parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// middleware for static content
+app.use(express.static('public'));
+
+// middleware for routing
+app.use(routes);
+
+
+// listener for PORT
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
