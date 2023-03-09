0854f352e859a30afd5a9d05937f9e7141155a76
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-08 19:42:02 -0500

    adds post request for api/notes

--- routes/htmlgets.js
+++ routes/htmlgets.js
@@ -1,14 +1,17 @@
+// dependencies for html gets
 const getRequest = require('express').Router();
 const path = require('path');
 
 // getRequest for index.html
-getRequest.get('*', (req, res) =>
+getRequest.get('*', (req, res) => {
+  console.info(`${req.method} request received for index.html`);
   res.sendFile(path.join(__dirname, '/public/assets/index.html'))
-)
+})
 
 // getRequest for notes.html
-getRequest.get('/notes', (req, res) =>
+getRequest.get('/notes', (req, res) => {
+  console.info(`${req.method} request received for notes.html`);
   res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
-)
+});
 
 module.exports = getRequest;
---
+++ routes/notesapi.js
@@ -0,0 +1,52 @@
+// dependencies for notes API
+const notesAPI = require('express').Router();
+const fs = require('fs');
+const dbData = require('../db/db.json')
+
+// get request for notesAPI that returns concents of db.json as JSON
+notesAPI.get('api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes/api`);
+  
+  // returns db.JSON as JSON
+  res.json(dbData);
+});
+
+// post request for notes API
+notesAPI.post('api.notes', (req, res) =>{
+  console.info(`${req.method} request received for notes/api`);
+
+  // destructure request body
+  const { title, text } = req.body
+
+  // if statement for request contains proper parameters
+  if (title && text) {
+
+    const newNote = {
+      title,
+      text
+    }
+
+    // adds new note to existing data base array
+    dbData.push(newNote);
+    // stringifies the dbData to rewrite file
+    const updatedNotes = JSON.stringify(dbData , null, 2);
+
+    // overwrites file with new notes including submitted one
+    fs.writeFile('../db/db.json', updatedNotes , (err) =>
+      // should probably throw an error here if it fails
+      err ? console.error(err) : console.info(`Review for ${newNote.title} has been added`)
+    )
+
+    // response object containing newNote
+    const response = {
+      status: 'success',
+      body: newNote
+    }
+
+    // response sent if successfully executed
+    res.status(201).json(response);
+  } else {
+    // yes I know I should use a different error code (probably 400)
+    res.status(418).json('Teapots need to have a title or text to be submitted')
+  }
+});
